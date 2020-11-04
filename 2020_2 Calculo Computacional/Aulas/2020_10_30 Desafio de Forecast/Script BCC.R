0c7f49a0c4ccb76f94e7287565c357a7bd0b5bfc
marcelo-bonora <marcelo.bonora@bandtec.com.br>
2020-11-04 10:47:59 -0200

    04/11/2020 - Desafio de Calculo BBC finalizado

--- 2020_2 Calculo Computacional/Aulas/2020_10_30 Desafio de Forecast/Script BCC.R
+++ 2020_2 Calculo Computacional/Aulas/2020_10_30 Desafio de Forecast/Script BCC.R
@@ -1,8 +1,8 @@
 #IMPORTE O TABELA PRIMEIRO
 
 #PLOTANDO O BARPLOT:
-barplot(BillingCloudCost$Valor~BillingCloudCost$Mês, main= "Desafio Billing Cloud Cost",
-ylab = "Valores($)", xlab = "Meses", col =c("#2195ec","#2195ec","#2195ec","#2195ec",
+barplot(BillingCloudCost$Valor~BillingCloudCost$IdLeitura, main= "Desafio Billing Cloud Cost",
+ylab = "Valores($)", xlab = "Meses", names.arg = c(BillingCloudCost$Mês), col = c("#2195ec","#2195ec","#2195ec","#2195ec",
 "#2195ec","#2195ec","#2195ec","#2195ec","#2195ec", "#2195ec"))
 
 #PUXANDO OS COEFICIENTES DA FUNÇÃO
@@ -14,6 +14,7 @@ x <- 11
 a <- 0.4727
 b <- 63
 y <- floor(a*x+b)
+y
 }
 
 #INSERINDO NOVA LINHA NA COLUNA
@@ -25,14 +26,14 @@ BillingCloudCost
 #PLOTANDO O BARPLOT COM O FORECAST DE NOVEMBRO
 {
 barplot(BillingCloudCost$Valor~BillingCloudCost$Mês,beside=TRUE, main= "Desafio Billing Cloud Cost",
-ylab = "Valores($)", xlab = "Meses", col =c("#2195ec","#2195ec","#2195ec","#2195ec",
+ylab = "Valores($)", xlab = "Meses",names.arg = c(BillingCloudCost$Mês), col =c("#2195ec","#2195ec","#2195ec","#2195ec",
 "#2195ec","#2195ec","#2195ec","#2195ec","#2195ec","#2195ec", "#00FF7F"))
 legend(x=11.5, y=100, xpd = TRUE, legend=c("Histórico", "Forecast"),
 fill=c("#2195ec", "#00FF7F"), bty="n")
 }
 
 #DISTRIBUIÇÃO DESSE VALOR
-
+{
 EC2 <- ceiling((35.86*100)/ 57) / 100
 EBS <- floor((12*100)/57) / 100
 S3 <- ceiling((9*100)/57) / 100
@@ -43,3 +44,4 @@ cores <- c("#2195ec","#00FF7F","#FFD700")
 
 pie(NOV_GASTOS, main = "Gastos em Novembro", labels = NOV_GASTOS, col = cores)
 legend("topright", fill = cores, legend = c("EC2","EBS","S3"))
+}
